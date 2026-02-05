55fb60f652acab60e4b014f63b15929bc4acffea
Arbaz Naeem <arbaznaeem555@gmail.com>
2026-02-05 23:16:45 +0500

    added main.tf

--- project-01/main.tf
+++ project-01/main.tf
@@ -1,1 +1,20 @@
+locals {
+  environment = "prod"
+}
 
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_details.name
+  location = var.resource_group_details.location
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = var.storge_account_details.name
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = var.storge_account_details.account_tier
+  account_replication_type = var.storge_account_details.account_replication_type
+
+  tags = {
+    environment = local.environment
+  }
+}
