b7687c922a75f5f94150206d89618c422370bee0
Arbaz Naeem <arbaznaeem555@gmail.com>
2026-02-11 03:35:35 +0500

    added autoscale.tf

---
+++ Production-Grade_Azure_Infrastructure+With _AutoScaling/terraform_files/autoscale.tf
@@ -0,0 +1,61 @@
+# create autoscale resource that will decrease the number of instances if the azurerm_orchestrated_scale set cpu usaae is below 10% for 2 minutes
+resource "azurerm_monitor_autoscale_setting" "autoscale" {
+  name                = "vmss-autoscale"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  target_resource_id  = azurerm_orchestrated_virtual_machine_scale_set.vmss_terraform_tutorial.id
+  enabled             = true
+
+  profile {
+    name = "cpu-based-autoscale"
+
+    capacity {
+      default = 3
+      minimum = 1
+      maximum = 10
+    }
+
+    # ⬇️ SCALE IN RULE (Decrease VMs)
+    rule {
+      metric_trigger {
+        metric_name        = "Percentage CPU"
+        metric_resource_id = azurerm_orchestrated_virtual_machine_scale_set.vmss_terraform_tutorial.id
+        time_grain         = "PT1M"   # metric every 1 min
+        statistic          = "Average"
+        time_window        = "PT2M"   # for 2 minutes
+        time_aggregation   = "Average"
+        operator           = "LessThan"
+        threshold          = 10
+      }
+
+      scale_action {
+        direction = "Decrease"
+        type      = "ChangeCount"
+        value     = "1"      # decrease by 1 VM
+        cooldown  = "PT5M"   # wait 5 mins before next action
+      }
+    }
+
+    # ⬆️ SCALE OUT RULE (Recommended, warna sirf decrease karega)
+    rule {
+      metric_trigger {
+        metric_name        = "Percentage CPU"
+        metric_resource_id = azurerm_orchestrated_virtual_machine_scale_set.vmss_terraform_tutorial.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "GreaterThan"
+        threshold          = 70
+      }
+
+      scale_action {
+        direction = "Increase"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT5M"
+      }
+    }
+  }
+}
+
