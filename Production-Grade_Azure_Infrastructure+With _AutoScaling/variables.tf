492be21d08399384a9793ca4d130bdc778539a76
Arbaz Naeem <arbaznaeem555@gmail.com>
2026-02-10 23:52:01 +0500

    added variable.tf

---
+++ Production-Grade_Azure_Infrastructure+With _AutoScaling/variables.tf
@@ -0,0 +1,10 @@
+variable "service_principal" {
+  description = "Azure Service Principal credentials"
+  type        = object({
+    client_id       = string
+    client_secret   = string
+    tenant_id       = string
+    subscription_id = string
+  })
+}
+
