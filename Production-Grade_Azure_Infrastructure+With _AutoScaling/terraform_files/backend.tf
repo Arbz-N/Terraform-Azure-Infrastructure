bd3e5d92d0e34b5c6679859762fd88f25962503b
Arbaz Naeem <arbaznaeem555@gmail.com>
2026-02-11 22:50:17 +0500

    added backend.tf

---
+++ Production-Grade_Azure_Infrastructure+With _AutoScaling/terraform_files/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "<TF_STATE_RESOURCE_GROUP>"
+    storage_account_name = "<TF_STATE_STORAGE_ACCOUNT>"
+    container_name       = "<TF_STATE_CONTAINER>"
+    key                  = "dev.terraform.tfstate"
+  }
+}
+
