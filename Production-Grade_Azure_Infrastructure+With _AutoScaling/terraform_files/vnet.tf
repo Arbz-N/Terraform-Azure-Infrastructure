2055959c427c128237943d187deff99078ba8dd0
Arbaz Naeem <arbaznaeem555@gmail.com>
2026-02-11 22:59:38 +0500

    updated output.tf

--- Production-Grade_Azure_Infrastructure+With _AutoScaling/terraform_files/vnet.tf
+++ Production-Grade_Azure_Infrastructure+With _AutoScaling/terraform_files/vnet.tf
@@ -25,40 +25,51 @@ resource "azurerm_network_security_group" "nsg" {
   resource_group_name = azurerm_resource_group.rg.name
 
   security_rule {
-    name                       = "allow-http"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "80"
-    source_address_prefix      = "*"
+    name = "allow-http-from-lb"
+    priority = 100
+    direction = "Inbound"
+    access = "Allow"
+    protocol = "Tcp"
+    source_port_range = "*"
+    destination_port_range = "80"
+    source_address_prefix = azurerm_public_ip.lb_pip.ip_address
     destination_address_prefix = "*"
   }
-
   security_rule {
-    name                       = "allow-https"
+    name                       = "allow-https-from-lb"
     priority                   = 101
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "443"
-    source_address_prefix      = "*"
+    source_address_prefix      = azurerm_public_ip.lb_pip.ip_address
     destination_address_prefix = "*"
   }
-
+  #ssh security rule
   security_rule {
-    name                       = "allow-ssh"
+    name                       = "allow-ssh-from-nat"
     priority                   = 102
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "22"
-    source_address_prefix      = "*"
+    source_address_prefix      = azurerm_public_ip.natgw_pip.ip_address
     destination_address_prefix = "*"
   }
+
+  security_rule {
+  name                       = "allow-outbound-internet"
+  priority                   = 200
+  direction                  = "Outbound"
+  access                     = "Allow"
+  protocol                   = "*"
+  source_port_range          = "*"
+  destination_port_range     = "*"
+  destination_address_prefix = "*"
+}
+
 }
 
 resource "azurerm_subnet_network_security_group_association" "nsg_assoc" {
