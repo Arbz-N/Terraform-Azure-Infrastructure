f4f7103c04ff4ef5479050824b9b9575652628b5
Arbaz Naeem <arbaznaeem555@gmail.com>
2026-02-15 22:59:09 +0500

    added vnet.tf

---
+++ Azure_VNET_Peering/vnet.tf
@@ -0,0 +1,14 @@
+resource "azurerm_virtual_network" "vnet1" {
+  name                = "vnet-1"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_virtual_network" "vnet2" {
+  name                = "vnet-2"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  address_space       = ["10.1.0.0/16"]
+}
+
