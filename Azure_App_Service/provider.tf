a161aa51d6f6dab94f0da158bc6afd872bc97a6e
Arbaz Naeem <arbaznaeem555@gmail.com>
2026-02-17 23:51:25 +0500

    added provider.tf in azure_app_service

---
+++ Azure_App_Service/provider.tf
@@ -0,0 +1,17 @@
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>4.32.0"
+    }
+  }
+  required_version = ">=1.9.0"
+}
+provider "azurerm" {
+  features {}
+  subscription_id = var.service_principal.subscription_id
+  client_id = var.service_principal.client_id
+  client_secret = var.service_principal.client_secret
+  tenant_id = var.service_principal.tenant_id
+}
