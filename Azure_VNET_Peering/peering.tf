6c77033561c774c9ab3cf9e32ac3c2d457784f6f
Arbaz Naeem <arbaznaeem555@gmail.com>
2026-02-19 02:03:36 +0500

    updated peering.tf in Azure_Vnet_peering

--- Azure_VNET_Peering/peering.tf
+++ Azure_VNET_Peering/peering.tf
@@ -1,7 +1,7 @@
 resource "azurerm_virtual_network_peering" "peerings" {
   for_each = var.peering
 
-
+  depends_on = [azurerm_virtual_network.vnet]
   name                      = each.key
   resource_group_name       = azurerm_resource_group.rg.name
   virtual_network_name      = azurerm_virtual_network.vnet[each.value.source].name
