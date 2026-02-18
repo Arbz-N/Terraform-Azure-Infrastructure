a07a6e2e2cc5c0bea215e4fe145e3d2c2bac7b81
Arbaz Naeem <arbaznaeem555@gmail.com>
2026-02-18 23:57:09 +0500

    updated vnets.tf in Azure_Vnet_peering

--- Azure_VNET_Peering/vnet.tf
+++ Azure_VNET_Peering/vnet.tf
@@ -1,14 +1,9 @@
-resource "azurerm_virtual_network" "vnet1" {
-  name                = var.vnet-1_details.name
+resource "azurerm_virtual_network" "vnet" {
+  for_each = var.vnets
+  name                = each.value.name
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  address_space       = [var.vnet-1_details.address_space]
+  address_space       = [each.value.address_space]
 }
 
-resource "azurerm_virtual_network" "vnet2" {
-  name                = var.vnet-2_details.name
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  address_space       = var.vnet-2_details.address_space
-}
 
