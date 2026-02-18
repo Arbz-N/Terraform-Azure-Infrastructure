177356c78ccefee6cfe8ef6fbc6ffa27ec5e1a5a
Arbaz Naeem <arbaznaeem555@gmail.com>
2026-02-18 23:39:50 +0500

    updated variables.tf in Azure_Vnet_peering

--- Azure_VNET_Peering/variables.tf
+++ Azure_VNET_Peering/variables.tf
@@ -8,4 +8,15 @@ variable "service_principal" {
   })
 }
 
-
+variable "vnet-1_details" {
+  type = object({
+    name = string
+    address_space = string
+  })
+}
+variable "vnet-2_details" {
+  type = object({
+    name = string
+    address_space = string
+  })
+}
