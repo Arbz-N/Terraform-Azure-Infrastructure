4c081155efe3f6a0c61a2ede98c6171072153ea7
Arbaz Naeem <arbaznaeem555@gmail.com>
2026-02-19 02:08:44 +0500

    updated terraform.tf in Azure_Vnet_peering

--- Azure_VNET_Peering/terraform.tf
+++ Azure_VNET_Peering/terraform.tf
@@ -1,9 +1,18 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "<TF_STATE_RESOURCE_GROUP>"
-    storage_account_name = "<TF_STATE_STORAGE_ACCOUNT>"
-    container_name       = "<TF_STATE_CONTAINER>"
-    key                  = "dev.terraform.tfstate"
+    resource_group_name  = "<YOUR_TFSTATE_RG_NAME>"
+    storage_account_name = "<YOUR_STORAGE_ACCOUNT_NAME>"
+    container_name       = "<YOUR_CONTAINER_NAME>"
+    key                  = "<ENVIRONMENT>.terraform.tfstate"
   }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.32.0"
+    }
+  }
+
+  required_version = ">= 1.9.0"
 }
 
