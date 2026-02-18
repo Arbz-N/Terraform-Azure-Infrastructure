ec77049ca2b0e3ba5b7a7f6efd16ffa7115ff44e
Arbaz Naeem <arbaznaeem555@gmail.com>
2026-02-19 02:21:48 +0500

    updated variables.tf in Azure_Vnet_peering

--- Azure_VNET_Peering/variables.tf
+++ Azure_VNET_Peering/variables.tf
@@ -1,17 +1,17 @@
 variable "service_principal" {
   description = "Azure Service Principal credentials"
-  type        = map(object({
+  type = object({
     client_id       = string
     client_secret   = string
     tenant_id       = string
     subscription_id = string
-  }))
+  })
 }
 
 variable "vnets" {
   description = "All VNETS Configurations"
   type = map(object({
-    name = string
+    name          = string
     address_space = string
   }))
 }
@@ -43,38 +43,61 @@ variable "vms" {
     })
 
     environment = string
-    nic_id      = string
+    nic_key     = string
   }))
 }
 
 variable "vnets_subnet" {
   type = map(object({
-    name = string
-    address_prefixes = string
+    name              = string
+    address_prefixes  = string
+    vnet_name         = string
   }))
 }
+
 variable "bastion_subnet" {
   type = map(object({
-    name = string
-    address_prefixes = string
+    name              = string
+    address_prefixes  = string
+    vnet_name         = string
   }))
 }
+
 variable "resource_group" {
   type = map(object({
     name     = string
     location = string
   }))
 }
+
 variable "peering" {
   type = map(object({
     source = string
     target = string
   }))
 }
+
 variable "nics" {
   type = map(object({
     ip_name                       = string
     private_ip_address_allocation = string
     subnet_key                    = string
+  }))
+}
+
+variable "ips_for_bastion" {
+  type = map(object({
+    name              = string
+    allocation_method = string
+    sku               = string
+  }))
+}
+
+variable "bastion_vms" {
+  type = map(object({
+    vm_name        = string
+    ip_name        = string
+    subnet_key     = string
+    ip_address_key = string
   }))
 }
