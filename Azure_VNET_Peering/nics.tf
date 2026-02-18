a2b4cecbe322c43aa5cc182ffb6e66a040033d23
Arbaz Naeem <arbaznaeem555@gmail.com>
2026-02-19 00:52:58 +0500

    updated nics.tf in Azure_Vnet_peering

--- Azure_VNET_Peering/nics.tf
+++ Azure_VNET_Peering/nics.tf
@@ -1,24 +1,13 @@
+resource "azurerm_network_interface" "nics" {
+  for_each = var.nics
 
-resource "azurerm_network_interface" "nic_vm1" {
-  name                = "nic-vm1"
   location            = azurerm_resource_group.rg.location
+  name                = each.key
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
-    name                          = "ipconfig1"
-    subnet_id                     = azurerm_subnet.vnet1_subnet1.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-resource "azurerm_network_interface" "nic_vm2" {
-  name                = "nic-vm2"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                          = "ipconfig1"
-    subnet_id                     = azurerm_subnet.vnet2_subnet1.id
-    private_ip_address_allocation = "Dynamic"
+    name                          = each.value.ip_name
+    private_ip_address_allocation = each.value.private_ip_address_allocation
+    subnet_id                     = azurerm_subnet.vnets_subnet[each.value.subnet_key].id
   }
 }
