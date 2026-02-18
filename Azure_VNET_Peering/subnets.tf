12e2aa689bfce4568304001d585245b30cae8cda
Arbaz Naeem <arbaznaeem555@gmail.com>
2026-02-19 02:06:39 +0500

    updated subnets.tf in Azure_Vnet_peering

--- Azure_VNET_Peering/subnets.tf
+++ Azure_VNET_Peering/subnets.tf
@@ -1,18 +1,19 @@
-# VNET1 Subnets
+# Application Subnets
 resource "azurerm_subnet" "vnets_subnet" {
   for_each = var.vnets_subnet
+
   name                 = each.value.name
   resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
+  virtual_network_name = azurerm_virtual_network.vnet[each.value.vnet_name].name
   address_prefixes     = [each.value.address_prefixes]
 }
 
+# Bastion Subnets
 resource "azurerm_subnet" "bastion_subnet" {
   for_each = var.bastion_subnet
+
   name                 = each.value.name
   resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
+  virtual_network_name = azurerm_virtual_network.vnet[each.value.vnet_name].name
   address_prefixes     = [each.value.address_prefixes]
 }
-
-
