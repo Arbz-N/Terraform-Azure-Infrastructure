871e9353c6d1ee689c8c08e315e7901f65eb13f8
Arbaz Naeem <arbaznaeem555@gmail.com>
2026-02-25 23:39:31 +0500

    updated resources.tf in Deploying_AKS_Cluster_With_Custom_Modules\aks

--- Deploying_AKS_Cluster_With_Custom_Modules/Modules/aks/resources.tf
+++ Deploying_AKS_Cluster_With_Custom_Modules/Modules/aks/resources.tf
@@ -1,74 +1,75 @@
-# Generate an SSH private key locally that will be used for AKS node login
-resource "tls_private_key" "ssh_key" {
+# Generate an SSH key pair locally for AKS node authentication
+resource "tls_private_key" "rsa_ssh_key" {
   algorithm = "RSA"
   rsa_bits  = 2048
 }
 
-# Fetch available AKS Kubernetes versions for the selected Azure region
+# Fetch the latest stable Kubernetes version available in the selected Azure region
 data "azurerm_kubernetes_service_versions" "current" {
   location        = var.location
   include_preview = false
 }
 
 # Create the Azure Kubernetes Service (AKS) cluster
-resource "azurerm_kubernetes_cluster" "aks" {
-
-  # Basic cluster settings
+resource "azurerm_kubernetes_cluster" "aks_cluster" {
+  name                = "practice-aks-cluster"
   location            = var.location
-  name                = "<AKS_CLUSTER_NAME>"
   resource_group_name = var.resource_group_name
-  dns_prefix          = "<DNS_PREFIX>"
+  dns_prefix          = "${var.resource_group_name}-cluster"
 
   kubernetes_version  = data.azurerm_kubernetes_service_versions.current.latest_version
 
-  # Managed resource group for AKS infrastructure
-  node_resource_group = "<NODE_RESOURCE_GROUP_NAME>"
+  node_resource_group = "${var.resource_group_name}-nrg"
 
-  # Default worker node pool configuration
+  oidc_issuer_enabled = true
+
   default_node_pool {
-    name                  = "defaultpool"
-    vm_size               = "<VM_SIZE>"
-    zones                 = [1,2,3]
+    name       = "defaultpool"
+    vm_size    = "Standard_D2s_v3"
+    zones      = [1, 3]
 
-    auto_scaling_enabled  = true
-    min_count             = 1
-    max_count             = 3
+    auto_scaling_enabled = true
+    min_count            = 1
+    max_count            = 3
 
-    os_disk_size_gb       = 30
-    type                  = "VirtualMachineScaleSets"
+    os_disk_size_gb = 30
+    type            = "VirtualMachineScaleSets"
 
     node_labels = {
-      "nodepool-type" = "<NODEPOOL_TYPE>"
-      "environment"   = "<ENVIRONMENT>"
+      "nodepool-type" = "system"
+      "environment"   = "prod"
       "nodepoolos"    = "linux"
     }
 
     tags = {
-      "nodepool-type" = "<NODEPOOL_TYPE>"
-      "environment"   = "<ENVIRONMENT>"
+      "nodepool-type" = "system"
+      "environment"   = "prod"
       "nodepoolos"    = "linux"
     }
   }
 
-  # Service principal used by AKS to manage Azure resources
   service_principal {
-    client_id     = "<AZURE_CLIENT_ID>"
-    client_secret = "<AZURE_CLIENT_SECRET>"
+    client_id     = var.client_id
+    client_secret = var.client_secret
   }
 
-  # Linux admin settings for nodes
   linux_profile {
-    admin_username = "<SSH_USERNAME>"
+    admin_username = "ubuntu"
 
     ssh_key {
-      key_data = tls_private_key.ssh_key.public_key_openssh
+      key_data = tls_private_key.rsa_ssh_key.public_key_openssh
     }
   }
 
-  # Cluster networking configuration
   network_profile {
     network_plugin    = "azure"
     load_balancer_sku = "standard"
   }
 }
 
+# Save the generated private key locally for SSH access
+resource "local_file" "private_key" {
+  content         = tls_private_key.rsa_ssh_key.private_key_pem
+  filename        = "${path.module}/aks-private-key.pem"
+  file_permission = "0600"
+}
