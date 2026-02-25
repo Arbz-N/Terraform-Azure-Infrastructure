a8fd04447d57e809b2859761262ece602df63a7b
Arbaz Naeem <arbaznaeem555@gmail.com>
2026-02-25 23:38:54 +0500

    updated outputs.tf in Deploying_AKS_Cluster_With_Custom_Modules\aks

--- Deploying_AKS_Cluster_With_Custom_Modules/Modules/aks/outputs.tf
+++ Deploying_AKS_Cluster_With_Custom_Modules/Modules/aks/outputs.tf
@@ -1,4 +1,12 @@
 output "config" {
-    value = azurerm_kubernetes_cluster.aks-cluster.kube_config_raw
+    value = azurerm_kubernetes_cluster.aks_cluster.kube_config_raw
 
 }
+output "private_key" {
+  value = tls_private_key.rsa_ssh_key.private_key_openssh
+  sensitive = true
+}
+
+output "public_key" {
+  value = tls_private_key.rsa_ssh_key.public_key_openssh
+ }
