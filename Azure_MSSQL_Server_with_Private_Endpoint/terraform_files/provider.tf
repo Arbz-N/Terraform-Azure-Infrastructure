fe07e9a703b201dddc49ccd39a01797145a37d46
Arbaz Naeem <arbaznaeem555@gmail.com>
2026-02-26 22:29:45 +0500

    added provider.tf into Azure_MSSQL_Server_with_Private_Endpoint

---
+++ Azure_MSSQL_Server_with_Private_Endpoint/terraform_files/provider.tf
@@ -0,0 +1,8 @@
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.service_principal.subscription_id   # <AZURE_SUBSCRIPTION_ID>
+  client_id       = var.service_principal.client_id          # <AZURE_CLIENT_ID>
+  client_secret   = var.service_principal.client_secret      # <AZURE_CLIENT_SECRET>
+  tenant_id       = var.service_principal.tenant_id          # <AZURE_TENANT_ID>
+}
