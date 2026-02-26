9847547af7cea793132c6f5681fe27e89364d907
Arbaz Naeem <arbaznaeem555@gmail.com>
2026-02-26 22:29:20 +0500

    added terraform.tf into Azure_MSSQL_Server_with_Private_Endpoint

---
+++ Azure_MSSQL_Server_with_Private_Endpoint/terraform_files/terraform.tf
@@ -0,0 +1,18 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "<YOUR_TFSTATE_RG_NAME>"
+    storage_account_name = "<YOUR_STORAGE_ACCOUNT_NAME>"
+    container_name       = "<YOUR_CONTAINER_NAME>"
+    key                  = "<ENVIRONMENT>.terraform.tfstate"
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.32.0"
+    }
+  }
+
+  required_version = ">= 1.9.0"
+}
+
