ea0ba786482ae927d62b6c879e79268db2be5dae
Arbaz Naeem <arbaznaeem555@gmail.com>
2026-02-27 23:51:30 +0500

    Updated main.tf file into terraform_files folder in Azure_App_Service_With_Terraform

--- Azure_App_Service_With_Terraform/Terraform_files/main.tf
+++ Azure_App_Service_With_Terraform/Terraform_files/main.tf
@@ -2,44 +2,52 @@ resource "azurerm_resource_group" "rg" {
   name = "${var.prefix}-rg"
   location = "canadacentral"
 }
-resource "azurerm_app_service_plan" "asp" {
+resource "azurerm_service_plan" "asp" {
   name                = "${var.prefix}-asp"
-  location            = "${azurerm_resource_group.rg.location}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
-  sku {
-    tier = "Standard"
-    size = "S1"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  os_type  = "Linux"
+  sku_name = "S1"
+}
+
+resource "azurerm_linux_web_app" "as" {
+  name                = "${var.prefix}-webapp"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_service_plan.asp.id
+
+  site_config {
+    always_on = false
   }
 }
 
-resource "azurerm_app_service" "as" {
-  name                = "${var.prefix}-webapp"
-  location            = "${azurerm_resource_group.rg.location}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
-  app_service_plan_id = "${azurerm_app_service_plan.asp.id}"
-}
-
-resource "azurerm_app_service_slot" "slot" {
+resource "azurerm_linux_web_app_slot" "slot" {
   name                = "${var.prefix}-staging"
-  location            = "${azurerm_resource_group.rg.location}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
-  app_service_plan_id = "${azurerm_app_service_plan.asp.id}"
-  app_service_name    = "${azurerm_app_service.as.name}"
+  app_service_id      = azurerm_linux_web_app.as.id
+
+
+  site_config {
+    always_on = false
+  }
+
 }
 
 resource "azurerm_app_service_source_control" "scm" {
-  app_id   = azurerm_app_service.as.id
-  repo_url = "https://github.com/piyushsachdeva/tf-sample-bg"
+  app_id   = azurerm_linux_web_app.as.id
+  repo_url = "https://github.com/Arbz-N/Simple_Testing_App.git"
   branch   = "master"
+  use_manual_integration = true
 }
 
 resource "azurerm_app_service_source_control_slot" "scm1" {
-  slot_id   = azurerm_app_service_slot.slot.id
-  repo_url = "https://github.com/piyushsachdeva/tf-sample-bg"
-  branch   = "appServiceSlot_Working_DO_NOT_MERGE"
+  slot_id   = azurerm_linux_web_app_slot.slot.id
+  repo_url = "https://github.com/Arbz-N/Simple_Testing_App.git"
+  branch   = "staging"
+  use_manual_integration = true
 }
 
 resource "azurerm_web_app_active_slot" "active" {
-  slot_id = azurerm_app_service_slot.slot.id
+  slot_id = azurerm_linux_web_app_slot.slot.id
 
 }
